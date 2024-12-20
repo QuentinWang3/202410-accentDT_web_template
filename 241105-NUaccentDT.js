3ad8a6896a1a7080954b8e10d3153ed428a6fb5d
QuentinWang3 <67474587+QuentinWang3@users.noreply.github.com>
2024-12-20 14:32:18 +0900

    Debugggggg

--- 241105-NUaccentDT.js
+++ 241105-NUaccentDT.js
@@ -1,18 +1,17 @@
 /*
 - goal: This JS file is designed for a accent discrimination task used to probe L2 learners' capability to differentiate different L2 English accents as well as the difference between native speakers' accent and L2 English accents.
 - created: 2024-11-05
-- modified: 2024-12-05
+- last modified: 2024-12-20
 - change log:
     2024-11-28
     - (done) preload at one time
     - (done) mark different phases (data:{phase: prac/experiment})
     2024-12-05
-    - add break time
+    - (done) add break time
     - (done) description of "訛りの発音" (accented speech)
     - (done) add informed consent
-    - add background information collection
-    TODO
-    - move experiment to github (complicated)
+    - (done) add background information collection
+    - (done) move experiment to github 
 
 */
 
@@ -20,6 +19,7 @@
 
 const jsPsych = initJsPsych({
     use_webaudio:false,
+    show_progress_bar:true,
     on_finish: function(){
         jsPsych.data.displayData();
     },
@@ -36,14 +36,19 @@ prac_timeline_variable.map(function(obj){
     return [obj.Sound1, obj.Sound2];
 }).flat(1);
 
-var main_audio_preload = 
-main_timeline_variable.map(function(obj){
+var sent_audio_preload = 
+sent_timeline_variable.map(function(obj){
+    return [obj.Sound1, obj.Sound2];
+}).flat(1);
+
+var word_audio_preload = 
+word_timeline_variable.map(function(obj){
     return [obj.Sound1, obj.Sound2];
 }).flat(1);
 
 //done - preload practice & main trial at the same time
     //concatenate two preload dataset
-var all_audio_preload = prac_audio_preload.concat(main_audio_preload);
+var all_audio_preload = prac_audio_preload.concat(sent_audio_preload, word_audio_preload);
 
 
 var preload = {
@@ -51,6 +56,76 @@ var preload = {
     audio: all_audio_preload,
 };
 
+//basic information collection
+
+var name_input = {
+    type: jsPsychSurveyText,
+    questions: [
+      {prompt: `
+      <div style="display: flex; align-items: center;">
+        <span style="white-space: nowrap; margin-right: 10px; font-size: 30px; line-height: 1.5">お名前をローマ字で入力してください：</span>
+      </div>`, placeholder: "Yamada Taro", name: 'participant_name', required: true}
+    ],
+    on_finish: function(data) {
+      // 将姓名保存到全局数据中
+      jsPsych.data.addProperties({
+        participant_name: data.response.participant_name
+      });
+      
+      // 动态更新 pavlovia_finish 的 participantID
+      pavlovia_finish.participantID = data.response.participant_name;
+    }
+};
+
+//volume test trial
+var vol_test_audio = {
+    type: jsPsychHtmlButtonResponse,
+    stimulus:`
+    <h1>
+    実験を始める前に、音量調節をお願いいたします。
+    </h1>
+    <p>
+    適切な音量に調節するまで、下の「再生」ボタンを押して、音量を確認しながら調節してください。
+    </p>
+
+    <audio id="vol-test-stimulus" src="stimuli/word/FAS_E1M-LNTW-01-16.wav"></audio>
+    <button id="play-button">再生する</button>
+    
+    <p>
+    
+    音量が自分の好みになるまで何回でも再生可能です。
+    「この音量で一番聞き取れやすくて、負担がない」と自分にとって最適な音量に調節できたら、
+    
+    下のボタンを押して、次のページで聞いた内容を入力してください。
+    音量がちゃんと聞こえるように調節したと確認のためなので、少し間違っていても構いません。
+    </p>
+  `,
+  choices: ['入力画面に進む'],
+  on_load: function() {
+    var audioElement = document.getElementById('vol-test-stimulus');
+    var playButton = document.getElementById('play-button');
+
+    playButton.addEventListener('click', function() {
+      audioElement.play();
+    });
+  }
+};
+  
+var vol_test_input = {
+    type: jsPsychSurveyText,
+    questions: [
+      {prompt: `
+      <div style="display: flex; align-items: center;">
+        <span style="white-space: nowrap; margin-right: 10px; font-size: 30px; line-height: 1.5">
+        聞いた英単語を入力してください：
+        </span>
+      </div>`, placeholder:"the English word you've heard", name: "vol-word", required: true, rows: 3, columns: 50}
+    ]
+};
+
+var vol_test = {
+    timeline: [vol_test_audio, vol_test_input],
+};
 
 //fullscreen before experiment
 var fullscreen = {
@@ -79,20 +154,24 @@ var title = {
     `,
     choices: [],
     trial_duration: 2000,
+    
 };
+
+
 
 
 var instruction_1 = {
     type: jsPsychHtmlButtonResponse,
     stimulus: `
     <h1>
-    インストラクション
+    インストラクション：この実験では何をするの？
     </h1>
-    <div>
     <p style="width:100%">
     この実験では２つの英語の音声がペアで流されます。
 
-    流された２つの音声を聞き、
+    音声の内容は少し早く見られるように設定されています。
+
+    音声の内容を見ながら、流された２つの音声を聞き、
 
     違う訛りの英語発音ペアであれば<span style="background-color: yellow;">[ F ]キー</span>を、
 
@@ -100,10 +179,9 @@ var instruction_1 = {
 
     この実験では同じ発音／違う発音の判断をする際の正答率が記録されます。
     </p>
-    </div>
     `,
     choices: ['「訛りの発音」次のページへ'],
-}
+};
 
 var instruction_2 = {
     type: jsPsychHtmlButtonResponse,
@@ -111,43 +189,49 @@ var instruction_2 = {
     <h1>
     インストラクション：訛りの発音とは？
     </h1>
-    <div>
-    <p>
+    <p style="width: 70%">
     以下の内容に対するご理解は、本実験の結果に重要な影響がありますので、読んでいただけますよう、よろしくお願いいたします。
 
     本実験では、「訛り」というのは、母語話者の発音のバリエーションではなく、母語が異なる話者が持つ「母語訛り」のことを指しています。母語話者の発音は地域によって、多くのバリエーションが存在しますが、本研究が重視しているのは母語話者と非母語話者の英語発音の違い、かつ母語が異なる非母語話者間の違いです。
 
     つまり、「違う訛りの英語発音ペア」というのは母語話者と非母語話者の英語発音ペア、あるいはA母語の非母語話者とB母語の非母語話者の英語発音ペアです。
 
-    上記の内容が理解できましたら、下のボタンを押すと、実験に関する説明に入ります。
+    上記の内容が理解できましたら、下のボタンを押すと、実験の手順に関する説明に入ります。
     </p>
-    </div>
     `,
     choices: ['理解できました、次のページへ'],
-}
+};
 
 
 var instruction_3 = {
     type: jsPsychHtmlButtonResponse,
     stimulus: `
     <h1>
-    実験に関するご説明
+    インストラクション：実験の手順に関するご説明
     </h1>
-    <div>
     <p>
     音声は常に凝視点（＋）の直後に流されますので、凝視点（＋）が提示されたら、必ずそのポイントに注目してください。
 
-    この発音知覚実験はおおむね４０分程度で終了の予定ですが、途中で休憩ポイントが1回あります。
+    この発音知覚実験はおおむね５０分程度で終了の予定ですが、途中で休憩ポイントが1回あります。
     休憩ポイントでは遠慮なく休憩してください（例：目を閉じるや深呼吸等）。
 
     ボタンを早く押すために、常に[ F ]と[ J ]のキーの上に指を置いてください。
     まず実験の手順に慣れていただくために、練習を１２問準備しました。
-    準備ができましたら、 [ J ] キーを押して練習へ進んでください。
+    準備ができましたら、 下のボタンを押して練習へ進んでください。
     </p>
-    </div>
     `,
-    choices: ['練習のセッションへ']
-}
+    choices: ['練習のセッションへ'],
+    data: {
+        phase: 'intro',
+        deviceInfo: {
+            userAgent: navigator.userAgent,
+            platform: navigator.platform,
+            language: navigator.language,
+            screenResolution: `${screen.width}x${screen.height}`,
+            windowSize: `${window.innerWidth}x${window.innerHeight}`
+        }
+    }
+};
 
 
 //block 1 - practice
@@ -163,44 +247,69 @@ var fixation = {
     trial_duration: 1000
 };
 
+//stimuli content 
+var sti_content = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus:  function() {
+        return `
+            <p style= "white-space: nowrap;text-align: center; padding: 20px; background-color: lightgray;">
+            音声の内容は「<strong>${jsPsych.timelineVariable('Content')}</strong>」 
+            </p>`;
+    },
+    choices: "NO_KEYS",
+    trial_duration: 500,
+};
+
 //play sound1 in prac
-var prac_sound1 = {
+var sound1 = {
     type: jsPsychAudioKeyboardResponse,
     stimulus: jsPsych.timelineVariable('Sound1'),
     choices: 'NO_KEYS',
-    prompt: `
-    <h1>練習：訛りの発音を聞いてください</h1>    
-    <div>
-    <p style="width: 100%"> 
-    違う訛りの英語発音であれば<span style="background-color: yellow;">[ F ]キー</span>を、同じ訛りの英語発音であれば <span style="background-color: yellow;">[ J ]キー</span>を<u>押してください</u>。
-    </p>`,
-    trial_ends_after_audio: true
+    prompt: function() {
+        return `
+        <p style= "white-space: nowrap;text-align: center; padding: 20px; background-color: lightgray;">
+        音声の内容は「<strong>${jsPsych.timelineVariable('Content')}</strong>」 
+        </p>`},
+    trial_ends_after_audio: true,
+};
+
+//Delay in prac
+var delay = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus:function() {
+        return `
+        <p style= "white-space: nowrap;text-align: center; padding: 20px; background-color: lightgray;">
+        音声の内容は「<strong>${jsPsych.timelineVariable('Content')}</strong>」 
+        </p>`},
+    choices: 'NO_KEYS',
+    trial_duration: 500,
 };
 
 //play sound2 in prac
 
-var prac_sound2 = {
+var sound2 = {
     type:jsPsychAudioKeyboardResponse,
     stimulus: jsPsych.timelineVariable('Sound2'),
     choices: 'NO_KEYS',
-    prompt: `
-    <h1>練習：訛りの発音を聞いてください</h1>    
-    <div>
-    <p style="width: 100%"> 
-    違う訛りの英語発音であれば<span style="background-color: yellow;">[ F ]キー</span>を、同じ訛りの英語発音であれば <span style="background-color: yellow;">[ J ]キー</span>を<u>押してください</u>。
-    </p>`,
+    prompt: function() {
+        return `
+        <p style= "white-space: nowrap;text-align: center; padding: 20px; background-color: lightgray;">
+        音声の内容は「<strong>${jsPsych.timelineVariable('Content')}</strong>」 
+        </p>`},
     trial_ends_after_audio: true
 };
 
 //response in prac (mark practice block as "practice" in response)
 var prac_response = {
     type: jsPsychHtmlKeyboardResponse,
-    stimulus: `
-    <h1>練習：弁別してください！</h1>    
-    <div>
+    stimulus: function() {
+        return `
+        <p style= "white-space: nowrap;text-align: center; padding: 20px; background-color: lightgray;">
+        音声の内容は「<strong>${jsPsych.timelineVariable('Content')}</strong>」 
+        </p>
     <p style="width: 100%"> 
     違う訛りの英語発音であれば<span style="background-color: yellow;">[ F ]キー</span>を、同じ訛りの英語発音であれば <span style="background-color: yellow;">[ J ]キー</span>を<u>押してください</u>。
-    </p>`,
+    </p>`},
     choices: ['f','j'],
     prompt: `
     <div style="display: flex; justify-content: space-around; margin-top: 20px;">
@@ -216,13 +325,17 @@ var prac_response = {
         phase: 'practice', //mark this part as practice session
         itemID: jsPsych.timelineVariable('ItemID'),
         stimulus_type: jsPsych.timelineVariable('type'),
-        correct_ans: jsPsych.timelineVariable('ExpAns')
+        correct_ans: jsPsych.timelineVariable('ExpAns'),
+        list: jsPsych.timelineVariable('List'),
+        content: jsPsych.timelineVariable('Content'),
+        sound1: jsPsych.timelineVariable('Sound1'),
+        sound2: jsPsych.timelineVariable('Sound2'),
     }
 };
 
 //timeline of practice session
 var prac_trial = {
-    timeline: [fixation, prac_sound1, prac_sound2, prac_response],
+    timeline: [fixation, sti_content, sound1, delay, sound2, prac_response],
     timeline_variables: prac_timeline_variable,
     randomize_order:true
 };
@@ -236,7 +349,7 @@ var prac_end = {
     練習はこれで終わりです。
     </h1>
     <p>
-    この時点でご質問等ありましたら、研究員にお尋ねください。
+    この時点でご質問等ありましたら、Webページを閉じずに、研究員にお尋ねください。
 
     実験の手順に関してご質問がないようでしたら、下のボタンを押すと、実験が始まります。
     </p>
@@ -245,44 +358,18 @@ var prac_end = {
 };
 
 
-//block 2 - main trial
-var main_sound1 = {
-    type: jsPsychAudioKeyboardResponse,
-    stimulus: jsPsych.timelineVariable("Sound1"),
-    choices: "NO_KEYS", 
-    prompt: `
-    <h1> 実験：訛りの発音を聞いてください </h1>
-    <div>
-    <p style="width: 100%"> 
-    違う訛りの英語発音であれば<span style="background-color: yellow;">[ F ]キー</span>を、同じ訛りの英語発音であれば <span style="background-color: yellow;">[ J ]キー</span>を<u>押してください</u>。
-    </p>
-    `,
-    trial_ends_after_audio: true
-};
+//block 2 - sentence trial
 
-
-var main_sound2 = {
-    type: jsPsychAudioKeyboardResponse,
-    stimulus: jsPsych.timelineVariable("Sound2"),
-    choices: "NO_KEYS", 
-    prompt: `
-    <h1> 実験：訛りの発音を聞いてください </h1>
-    <div>
-    <p style="width: 100%"> 
-    違う訛りの英語発音であれば<span style="background-color: yellow;">[ F ]キー</span>を、同じ訛りの英語発音であれば <span style="background-color: yellow;">[ J ]キー</span>を<u>押してください</u>。
-    </p>
-    `,
-    trial_ends_after_audio: true
-};
-
-var main_response = {
+var sent_response = {
     type: jsPsychHtmlKeyboardResponse,
-    stimulus: `
-    <h1> 実験：弁別してください！ </h1>
-    <div>
+    stimulus:  function() {
+        return `
+        <p style= "white-space: nowrap;text-align: center; padding: 20px; background-color: lightgray;">
+        音声の内容は「<strong>${jsPsych.timelineVariable('Content')}</strong>」 
+        </p>
     <p style="width: 100%"> 
     違う訛りの英語発音であれば<span style="background-color: yellow;">[ F ]キー</span>を、同じ訛りの英語発音であれば <span style="background-color: yellow;">[ J ]キー</span>を<u>押してください</u>。
-    </p>`,
+    </p>`},
     choices: ['f','j'],
     prompt: `
     <div style="display: flex; justify-content: space-around; margin-top: 20px;">
@@ -296,35 +383,113 @@ var main_response = {
     `,
     choices: ['f', 'j'],
     data: {
-        phase: "main",
-        itemID: jsPsych.timelineVariable('ItemID'),
+        phase: "sentence",
+        itemID: jsPsych.timelineVariable('SentenceID'),
         stimulus_type: jsPsych.timelineVariable('type'),
         correct_ans: jsPsych.timelineVariable('ExpAns'),
+        list: jsPsych.timelineVariable('List'),
+        content: jsPsych.timelineVariable('Content'),
+        sound1: jsPsych.timelineVariable('Sound1'),
+        sound2: jsPsych.timelineVariable('Sound2'),
     },
 };
 
-var main_trial = {
-    timeline: [fixation, main_sound1, main_sound2, main_response],
-    timeline_variables: main_timeline_variable,
+var sent_trial = {
+    timeline: [fixation, sti_content, sound1, delay, sound2, sent_response],
+    timeline_variables: sent_timeline_variable,
     randomize_order: true,
 };
+
+//break
+var breaktime = {
+    type: jsPsychHtmlButtonResponse,
+    stimulus: `
+    <h1>
+    休憩のお時間です！お疲れ様です！
+    </h1>
+    <p style="width:100%">
+    これまでの実験、いかがですか？
+
+    大変かと思いますが、あと少しで終わります。
+
+    少し体を動かしたり、水分補給をしたりして、５分以内の休憩を取りましょう。
+
+    休憩が終わりましたら、下のボタンを押して、実験を続けてください。
+    </p>
+    `,
+    choices: ['休憩完了です。'],
+    trial_duration: 300000, // 5mins
+    response_ends_trial:true,
+};
+
+//after break
+var after_break = {
+    type: jsPsychHtmlButtonResponse,
+    stimulus: `
+    <h1 style="width:100%">
+    実験に戻りましょう！
+    </h1>`,
+    choices: ['続けます！'],
+};
+
+
+//word session
+var word_response = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus:  function() {
+        return `
+        <p style= "white-space: nowrap;text-align: center; padding: 20px; background-color: lightgray;">
+        音声の内容は「<strong>${jsPsych.timelineVariable('Content')}</strong>」 
+        </p>
+    <p style="width: 100%"> 
+    違う訛りの英語発音であれば<span style="background-color: yellow;">[ F ]キー</span>を、同じ訛りの英語発音であれば <span style="background-color: yellow;">[ J ]キー</span>を<u>押してください</u>。
+    </p>`},
+    choices: ['f','j'],
+    prompt: `
+    <div style="display: flex; justify-content: space-around; margin-top: 20px;">
+        <div style="display: inline-block; padding: 15px 25px; margin-top: 20px; border: 2px solid #007bff; border-radius: 10px; background-color: #e7f3ff; color: #007bff; font-size: 18px;">
+            違うの場合は [F] キーを                   
+        </div>
+        <div style="display: inline-block; padding: 15px 25px; margin-top: 20px; border: 2px solid #007bff; border-radius: 10px; background-color: #e7f3ff; color: #007bff; font-size: 18px;">
+            同じの場合は [J] キーを                   
+        </div>
+    </div>
+    `,
+    choices: ['f', 'j'],
+    data: {
+        phase: "word",
+        itemID: jsPsych.timelineVariable('WordID'),
+        stimulus_type: jsPsych.timelineVariable('type'),
+        correct_ans: jsPsych.timelineVariable('ExpAns'),
+        list: jsPsych.timelineVariable('List'),
+        content: jsPsych.timelineVariable('Content'),
+        sound1: jsPsych.timelineVariable('Sound1'),
+        sound2: jsPsych.timelineVariable('Sound2'),
+    },
+};
+
+var word_trial = {
+    timeline: [fixation, sti_content, sound1, delay, sound2, word_response],
+    timeline_variables: word_timeline_variable,
+    randomize_order: true,
+};
+
 
 //ending
 
 var ending = {
     type: jsPsychHtmlButtonResponse,
     stimulus: `
-    <h1 style="text-align:left; width:100%;">
+    <h1 style="text-align:left; ">
     ご協力ありがとうございます。大変お疲れ様です。
     実験はこれで終わります。
     </h1>
-    <div>
     <p> 
-    お手数をおかけしますが、実験を完了した後、研究者にその旨をお知らせください。
+    お手数をおかけしますが、実験完了後、研究者にその旨をご連絡いただき、実験結果のファイルを添付してメールで送信してください。
 
-    また、ご質問がございましたら、いつでもご連絡がいただけましたら幸いです。
+    また、ご質問がございましたら、いつでもご連絡いただければ幸いです。
 
-    下のボタンを押していただいて、実験結果のファイルを保存してください。保存完了後、Webページを閉じていただいても大丈夫です。
+    下のボタンを押して、実験結果のファイルを保存してください。保存完了後は、Webページを閉じていただいても大丈夫です。
     </p>`,
     choices:["結果を保存します。"],
     response_ends_trial: true
@@ -339,9 +504,10 @@ var exit_fullscreen = {
 var pavlovia_finish = {
     type: jsPsychPavlovia,
     command: "finish",
+    participantID: "participant_name",
 };
 
 
-var timeline = [pavlovia_init, preload, fullscreen, title, instruction_1, instruction_2, instruction_3, prac_trial, prac_end, main_trial, pavlovia_finish, ending,  exit_fullscreen];
+var timeline = [pavlovia_init, preload, name_input, vol_test, fullscreen, title, instruction_1, instruction_2, instruction_3, prac_trial, prac_end, word_trial, breaktime, after_break, sent_trial, pavlovia_finish, ending,  exit_fullscreen];
 
 jsPsych.run(timeline);
--- 241105-stimuli_list_debug.js
+++ 241105-stimuli_list_debug.js
@@ -4,50 +4,78 @@
 
 //practice stimuli
 var prac_timeline_variable = [
-    {
-      "ItemID": 1,
-      "Sound1": "stimuli/FAS_E5M-BKBR-00-01.wav",
-      "Sound2": "stimuli/FAS_E1M-BKBR-00-01.wav",
-      "ExpAns": "j",
-      "type": "EE"
-    },
-    {
-      "ItemID": 2,
-      "Sound1": "stimuli/FAS_E1M-BKBR-00-02.wav",
-      "Sound2": "stimuli/FAS_E5M-BKBR-00-02.wav",
-      "ExpAns": "j",
-      "type": "EE"
-    },
-    {
-      "ItemID": 3,
-      "Sound1": "stimuli/FAS_E1M-BKBR-00-03.wav",
-      "Sound2": "stimuli/FAS_E5M-BKBR-00-03.wav",
-      "ExpAns": "j",
-      "type": "EE"
-    }
-   ];
-
-//experiment trial stimuli
-var main_timeline_variable = [
   {
+    "Content": "school",
+    "List": "WIPI",
+    "ListID": 1,
+    "ListNo": 1,
     "ItemID": 1,
-    "Sound1": "stimuli/FAS_E5M-BKBR-01-01.wav",
-    "Sound2": "stimuli/FAS_E1M-BKBR-01-01.wav",
+    "Sound1": "stimuli/FAS_E5M-WIPI-01-01.wav",
+    "Sound2": "stimuli/FAS_E1M-WIPI-01-01.wav",
     "ExpAns": "j",
     "type": "EE"
   },
   {
+    "Content": "ball",
+    "List": "WIPI",
+    "ListID": 1,
+    "ListNo": 2,
     "ItemID": 2,
+    "Sound1": "stimuli/FAS_E1M-WIPI-01-02.wav",
+    "Sound2": "stimuli/FAS_E5M-WIPI-01-02.wav",
+    "ExpAns": "j",
+    "type": "EE"
+  },
+];
+
+//sentence trial stimuli
+var sent_timeline_variable = [
+  {
+    "List": "BKB",
+    "ListID": "List 1",
+    "Content": "The clown had a funny face.",
+    "ListNo": 1,
+    "SentenceID": 1,
+    "Sound1": "stimuli/FAS_E1M-BKBR-01-01.wav",
+    "Sound2": "stimuli/FAS_E5M-BKBR-01-01.wav",
+    "type": "EE",
+    "ExpAns": "j"
+  },
+  {
+    "List": "BKB",
+    "ListID": "List 1",
+    "Content": "The car engine is running.",
+    "ListNo": 2,
+    "SentenceID": 2,
     "Sound1": "stimuli/FAS_E1M-BKBR-01-02.wav",
     "Sound2": "stimuli/FAS_E5M-BKBR-01-02.wav",
-    "ExpAns": "j",
-    "type": "EE"
+    "type": "EE",
+    "ExpAns": "j"
+  },];
+
+
+ // word trial stimuli
+ var word_timeline_variable = [
+  {
+    "List": "PBK",
+    "ListID": "List 1",
+    "Content": "please",
+    "ListNo": 1,
+    "WordID": 1,
+    "Sound1": "stimuli/word/FAS_E1M-PBKW-01-01.wav",
+    "Sound2": "stimuli/word/FAS_E5M-PBKW-01-01.wav",
+    "type": "EE",
+    "ExpAns": "j"
   },
   {
-    "ItemID": 3,
-    "Sound1": "stimuli/FAS_E5M-BKBR-01-03.wav",
-    "Sound2": "stimuli/FAS_E1M-BKBR-01-03.wav",
-    "ExpAns": "j",
-    "type": "EE"
-  }
- ];
+    "List": "PBK",
+    "ListID": "List 1",
+    "Content": "great",
+    "ListNo": 2,
+    "WordID": 2,
+    "Sound1": "stimuli/word/FAS_E5M-PBKW-01-02.wav",
+    "Sound2": "stimuli/word/FAS_E1M-PBKW-01-02.wav",
+    "type": "EE",
+    "ExpAns": "j"
+  },
+];
